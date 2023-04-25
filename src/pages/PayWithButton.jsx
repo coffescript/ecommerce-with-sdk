f04b4ac6830680397aa091f3909b7c88b69c2a0b
Deiby Mejia Ruiz <47683800+elmaedesistemas@users.noreply.github.com>
2023-04-25 11:26:49 -0600

    Update again PayWithButton.jsx

--- src/pages/PayWithButton.jsx
+++ src/pages/PayWithButton.jsx
@@ -83,6 +83,8 @@ const ItemDetails = () => {
             },
             amount: amount,
             currency: "CRC",
+            aplicaLineaCredito: true,
+            aplicaSeguroSaldoDeudor: true,
             baseRouteEcommerce: 'https://ecommerce-with-sdk.vercel.app/',
             detail: "this is just a test",
             onComplete: (data) => console.log('hello monge', data)
